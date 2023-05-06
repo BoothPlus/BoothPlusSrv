b7b5efefcf6675a6b70227fbbc90e0339417e823
ribeKim <ribe@ribe.io>
2023-05-06 20:03:10 +0900

    mysql로 변경

--- src/models/user.js
+++ src/models/user.js
@@ -6,13 +6,13 @@ const User = {}
 User.AddUser = async (userEntity) => {
   const { userId, name, email, profileImage } = userEntity
 
-  const searchUserSql = 'SELECT COUNT(1) AS COUNT FROM USERS WHERE USER_ID = :USER_ID'
-  const result = await connection.execute(searchUserSql, [userId])
+  const searchUserSql = 'SELECT COUNT(1) AS COUNT FROM USERS WHERE USER_ID = ?'
+  const [rows, fields] = await connection.execute(searchUserSql, [userId])
 
-  if (!!result.rows[0]['COUNT']) console.log('????')
-
-  const addUserSql = 'INSERT INTO USERS (USER_ID, NAME, EMAIL, PROFILE_IMAGE) VALUES (:USER_ID, :NAME, :EMAIL, :PROFILE_IMAGES)'
-  await connection.execute(addUserSql, [userId, name, email, profileImage])
+  if (rows[0]['COUNT'] === 0) {
+    const addUserSql = 'INSERT INTO USERS (USER_ID, NAME, EMAIL, PROFILE_IMAGE) VALUES (?, ?, ?, ?)'
+    await connection.execute(addUserSql, [userId, name, email, profileImage])
+  }
 }
 
 export default User
