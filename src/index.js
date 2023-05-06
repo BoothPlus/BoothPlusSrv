ad182ef08233adeeb8fdc9e004eab9a8c5c2cd31
ribeKim <ribe@ribe.io>
2023-05-06 17:56:45 +0900

    API 접근 시 인증 태우는 미들웨어 생성

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import express from 'express'
+import './config/dbConnection.js'
+import Auth from './config/auth.js'
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
+app.use((req, res, next) => {
+  const bearerHeader = req.headers['authorization']
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(' ')
+    const token = bearer[1]
+    Auth.TokenValidationCheck(token).then(res => {
+      if (res) next()
+    })
+  } else {
+    res.status(401).send('tokenExpired or Error Occurred')
+  }
+})
+
+app.get('/', async (req, res) => {
   res.send('asdad')
 })
 
 app.listen(port, () => {
   console.log(`App Listening Port => ${port}`)
 })
+
 
