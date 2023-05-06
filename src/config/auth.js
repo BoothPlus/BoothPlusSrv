5619d369efcc3d026ea58b411dfe41f5a8c8e254
ribeKim <ribe@ribe.io>
2023-05-06 18:01:33 +0900

    민감한 데이터 삭제

--- src/config/auth.js
+++ src/config/auth.js
@@ -7,7 +7,7 @@ Auth.TokenValidationCheck = async (token) => {
   const response = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${token}`)
   const data = await response.json()
 
-  const result = data['issued_to']?.startsWith(process.env.CHROME_EXTENSION_CLIENT_ID || '309877406534')
+  const result = data['issued_to']?.startsWith(process.env.CHROME_EXTENSION_CLIENT_ID)
 
   if (result) {
     const { id, name, email, picture } = await Auth.GetUserInfo(token)
